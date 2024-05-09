44b28a1740c97a5eacac7dbca63f71ad93b3351a
mohcinedev <mouhcinedev@gmail.com>
2024-05-09 09:53:59 +0100

    sort the if statment

--- handleArgs/checkArgs.go
+++ handleArgs/checkArgs.go
@@ -2,33 +2,32 @@ package handleArgs
 
 import (
 	"errors"
-	"strings"
 )
 
 func CheckArgs(myArgs []string) (error, []string) {
-	if len(myArgs) < 1 {
-		return usageMessage(), []string{}
-	} else if len(myArgs) > 2 {
+	if len(myArgs) < 1 || len(myArgs) > 2 {
 		return usageMessage(), []string{}
 	}
-
 	//// fs project
 
+	// if the file is provided
 	if len(myArgs) == 2 {
 
 		Banner := myArgs[1]
+ 
+		if Banner == "standard.txt" || Banner == "standard" {
+			return nil, []string{myArgs[0], "standard"}
 
-		if strings.Contains(Banner, ".") {
-			if Banner == "standard.txt" || Banner == "standard" {
-				return nil, []string{myArgs[0], "standard"}
-			} else if Banner == "shadow.txt" || Banner == "shadow" {
-				return nil, []string{myArgs[0], "shadow"}
-			} else if Banner == "thinkertoy.txt" || Banner == "thinkertoy" {
-				return nil, []string{myArgs[0], "thinkertoy"}
-			} else if Banner == "mine.txt" || Banner == "mine" {
-				return nil, []string{myArgs[0], "mine"}
-			}
-		}
+		} else if Banner == "shadow.txt" || Banner == "shadow" {
+			return nil, []string{myArgs[0], "shadow"}
+
+		} else if Banner == "thinkertoy.txt" || Banner == "thinkertoy" {
+			return nil, []string{myArgs[0], "thinkertoy"}
+
+		} else if Banner == "mine.txt" || Banner == "mine" {
+			return nil, []string{myArgs[0], "mine"}
+
+		} 
 	}
 	return nil, myArgs
 }
