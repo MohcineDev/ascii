a56e1349809a7374670bf1e4d05f55be6168280d
mohcinedev <mouhcinedev@gmail.com>
2024-05-12 16:47:08 +0100

    add main test

--- output/main_test.go
+++ output/main_test.go
@@ -1,11 +1,97 @@
 package main
 
 import (
-	"fmt"
+	"os"
+	"os/exec"
+	"strings"
 	"testing"
 )
 
-func testMain(t *testing.T) {
-	fmt.Println("1232")
+func TestMainOneArg(t *testing.T) {
+	cmd := exec.Command("./output", "1")
+	content, err := os.ReadFile("./test/one.txt")
+	if err != nil {
+		t.Fatalf("error")
+	}
+	output, _ := cmd.Output()
+	if !strings.EqualFold(string(content), string(output)) {
+		t.Fatalf("not equal")
+	}
 }
 
+func TestMainTwoArgWrongFile(t *testing.T) {
+	cmd := exec.Command("./output", "1", "a")
+
+	output, _ := cmd.Output()
+	if !strings.EqualFold("Error : a.txt file not found\n", string(output)) {
+		t.Fatalf("not equal")
+	}
+}
+
+func TestMainTwoArgsCorrectFile(t *testing.T) {
+	cmd := exec.Command("./output", "A", "shadow")
+
+	output, _ := cmd.Output()
+
+	content, err := os.ReadFile("./test/CorrectFile.txt")
+	if err != nil {
+		t.Fatalf("error")
+	}
+
+	if !strings.EqualFold(string(content), string(output)) {
+		t.Fatalf("not equal")
+	}
+}
+
+func TestMainTwoArgsCorrectFlag(t *testing.T) {
+	cmd := exec.Command("./output", "--output=res.txt", "shadow")
+
+	cmd.Output()
+
+	res, err := os.ReadFile("res.txt")
+	if err != nil {
+		t.Fatalf("error")
+	}
+	src, err0 := os.ReadFile("./test/CorrectFlag.txt")
+	if err0 != nil {
+		t.Fatalf("error")
+	}
+
+	if !strings.EqualFold(string(res), string(src)) {
+		t.Fatalf("not equal")
+	}
+}
+
+func TestMainTwoArgsIncorrectFlag(t *testing.T) {
+	cmd := exec.Command("./output", "T", "thinkertoy")
+
+	output, _ := cmd.Output()
+
+	src, err0 := os.ReadFile("./test/IncorrectFlag.txt")
+	if err0 != nil {
+		t.Fatalf("error")
+	}
+
+	if !strings.EqualFold(string(output), string(src)) {
+		t.Fatalf("not equal")
+	}
+}
+
+/////THREE ARGS
+
+func TestMainThreeArgs(t *testing.T) {
+	cmd := exec.Command("./output", "--output=./test/threeArgs.txt", "Hello", "thinkertoy")
+	cmd.Output()
+
+	res, err := os.ReadFile("./test/threeArgs.txt")
+	if err != nil {
+		t.Fatalf("error")
+	}
+	result, err := os.ReadFile("./test/threeArgsResult.txt")
+	if err != nil {
+		t.Fatalf("error")
+	}
+	if !strings.EqualFold(string(result), string(res)) {
+		t.Fatalf("not equal")
+	}	
+}
